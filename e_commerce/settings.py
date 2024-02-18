1e474b4489d108ba75f8aa546301b61f9069590e
unknown <bahatimarc2000@gmai..com>
2024-02-18 16:32:45 +0100

    cette foici c'est la fin le reste c'est du disign

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)d(ku8gig8=+s3h$vxnqr65zo*nz#cw3+qm(j2n(mog)$2@_7s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=True, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -149,8 +148,8 @@ MESSAGE_TAGS = {
 }
 
 #SHTP Configuration
-EMAIL_HOST= config('EMAIL_HOST')
-EMAIL_PORT=config('EMAIL_PORT',cast=int)
-EMAIL_HOST_USER= config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS =config('EMAIL_USE_TLS',cast=bool)
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER= 'bahatimarc2000@gmail.com'
+EMAIL_HOST_PASSWORD='qeiugnaofxudomgh'
+EMAIL_USE_TLS =True
